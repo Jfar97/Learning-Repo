609d3d1db4b84e839d9f7eb7c82682e63b98c1fb
Justin Faris <jwfaris@asu.edu>
2025-10-23 11:02:29 -0700

    Create FileToChange.py

---
+++ FileToChange.py
@@ -0,0 +1,7 @@
+# This is the file that you should modify by adding:
+#   A comment with your name and date of the change
+#   A print statement that prints out your name
+# Follow the example below:
+
+# Justin Faris - 23 October 2025
+print("Justin Faris")
